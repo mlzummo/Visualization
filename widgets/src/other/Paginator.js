532492650c61787e3cc187acb814ee9aa13a56a1
Mathew Zummo <mzummo@gmail.com>
2015-02-25 14:35:26 -0500

    Adds Pagination widget and modifies Table widget
    Signed-off-by: Mathew Zummo <mzummo@gmail.com>

---
+++ widgets/src/other/Paginator.js
@@ -0,0 +1,110 @@
+(function (root, factory) {
+    if (typeof define === "function" && define.amd) {
+        define(["../common/HTMLWidget","css!./Paginator"], factory);
+    } else {
+        root.Entity = factory(root.HTMLWidget);
+    }
+}(this, function (HTMLWidget) {
+    function Paginator() {
+        HTMLWidget.call(this);
+        this._class = "paginator";
+
+        this._tag = "div";
+        
+        this._pageNumber = 1
+        
+        this._onSelect;
+
+        this.np = 1;
+
+        this.pn = [];
+    };
+    
+    Paginator.prototype = Object.create(HTMLWidget.prototype);
+    
+    Paginator.prototype.publish("itemsPerPage", 2, "number", "Pagination items per page");
+    Paginator.prototype.publish("numItems", 10, "number", "Pagination total number of items");
+
+    Paginator.prototype.enter = function (domNode, element) {
+        HTMLWidget.prototype.enter.apply(this, arguments);
+
+        this.paginator = element.append("ul").attr("class","paginator pagination pagination-sm");     
+    };
+
+    Paginator.prototype.update = function (domNode, element) {
+        var context = this;
+        this.np = Math.ceil(this.numItems() / this.itemsPerPage()) || 1;
+
+        this.pn = [];
+        this.pn.push("previous");
+        for (var i=0; i < this.np; i++) {
+            this.pn.push(i+1);
+        }
+        this.pn.push("next");
+        
+        var page = this.paginator.selectAll("li").data(this.pn,function(d) { return d; });
+        var pageText = page 
+            .enter() 
+            .append("li") 
+            .append("a") 
+            .attr("href", "#")
+
+            .on('click', function(d, i) {
+
+                d3.event.preventDefault();
+                
+                if (d=='next') {
+
+                    if ((context._pageNumber+1) <= context.np) {
+      
+                        context._pageNumber++;  
+                        context._onSelect(context._pageNumber,"next"); 
+
+                    }
+
+                } else if (d=='previous') {
+
+                    if ((context._pageNumber-1) >= 1) {
+
+                        context._pageNumber--; 
+                        context._onSelect(context._pageNumber,"previous"); 
+
+                    }
+
+                } else {
+                    context._pageNumber = d; 
+                    context._onSelect(context._pageNumber);
+                }
+            });
+            
+        pageText.text(function(d) { return d; });
+        
+        page.exit().remove();
+        page.order();
+
+        // set active page
+        this.paginator.selectAll("li").classed("active", function(e, j) { return j == context._pageNumber; });
+
+        // delete all element(s) associated with paginator on removal
+        if (this.numItems() == 0) {
+            d3.select(domNode).remove();
+        }        
+    };
+    
+    Paginator.prototype.exit = function (domNode, element) {
+        HTMLWidget.prototype.exit.apply(this, arguments);
+    };
+
+    Paginator.prototype.itemsPerPage = function (_) {
+        if (!arguments.length) return this._itemsPerPage;
+        
+        if (this._itemsPerPage != _ ) { 
+            this._itemsPerPage = _; 
+            //this._pageNumber = 1;         
+        }
+
+        return this;
+    };
+
+    return Paginator;
+}));
--- widgets/src/other/Table.js
+++ widgets/src/other/Table.js
@@ -1,11 +1,11 @@
 "use strict";
 (function (root, factory) {
     if (typeof define === "function" && define.amd) {
-        define(["../common/HTMLWidget", "css!./Table"], factory);
+        define(["../common/HTMLWidget","../other/Paginator", "css!./Table"], factory);
     } else {
-        root.Entity = factory(root.HTMLWidget);
+        root.Entity = factory(root.HTMLWidget, root.Paginator);
     }
-}(this, function (HTMLWidget) {
+}(this, function (HTMLWidget, Paginator) {
     function Table() {
         HTMLWidget.call(this);
         this._class = "other_Table";
@@ -13,12 +13,22 @@
         this._tag = "table";
 
         this._columns = [];
+
+        this._pageNumber = 1; // TODO possibly handle this in the paginator instance itself?
+
+        this.paginator = new Paginator();    
     };
     Table.prototype = Object.create(HTMLWidget.prototype);
+
+    Table.prototype.publish("pagination", true, "boolean", "enable or disable pagination");
+    Table.prototype.publish("itemsPerPage", 2, "number", "Pagination items per page");
+    //Table.prototype.publish("numItems", 10, "number", "Pagination total number of items"); not needed
 
     Table.prototype.enter = function (domNode, element) {
         this.thead = element.append("thead").append("tr");
         this.tbody = element.append("tbody");
+        this.tfoot = element.append("tfoot");
+        //this.paginator.target(this.tfoot.node()); 
     };
 
     Table.prototype.update = function (domNode, element) {
@@ -36,7 +46,48 @@
             .remove()
         ;
 
-        var rows = this.tbody.selectAll("tr").data(this._data);
+
+        if (this.pagination()) {
+            if (this.paginator.target() == null) {
+                //this.paginator.target(this.tfoot.node());
+                var pnode = document.getElementById(this.id()).parentNode.id 
+                this.paginator.target(pnode); 
+                //this.paginator.target(domNode); 
+            }
+
+            this.paginator.itemsPerPage(this.itemsPerPage())
+            this.paginator.numItems(this._data.length)
+
+            this.paginator._onSelect = function(p, d) {
+                console.log('page: '+p);
+                context._pageNumber = p;
+                context.render();
+                return;
+            };
+
+            // NOTE: change of itemsPerPage will cause reset of page number by design due to glitch
+            
+        } else {
+            this.paginator.numItems(0);
+            this.paginator.itemsPerPage(0);
+            this.paginator._pageNumber = 1;
+        }
+        
+        // pageNumber starts at index 1
+        var pageNumber = this._pageNumber-1;
+        var itemsOnPage = this.itemsPerPage();
+        
+        var start = pageNumber * itemsOnPage;
+        var end = parseInt(pageNumber * itemsOnPage) + parseInt(itemsOnPage);
+        var tData;
+        
+        if (this.pagination()) {
+            tData = this._data.slice(start,end);
+        } else {
+            tData = this._data;
+        }
+
+        var rows = this.tbody.selectAll("tr").data(tData);
         rows
             .enter()
             .append("tr")
@@ -65,6 +116,9 @@
         cells.exit()
             .remove()
         ;
+
+        this.paginator.render();
+
     };
 
     Table.prototype.exit = function (domNode, element) {
@@ -73,6 +127,38 @@
     };
 
     Table.prototype.click = function (d) {
+    };
+
+    Table.prototype.itemsPerPage = function (_) {
+        if (!arguments.length) return this._itemsPerPage;
+
+        if (this._itemsPerPage !== _ && _ != 0 ) { 
+            this._itemsPerPage = _;
+
+            // todo fix page number 1 thing? 
+            this._pageNumber = 1;
+            this.paginator._pageNumber = 1;
+            //this.paginator.render();
+        }
+        
+        return this;
+    };
+
+    Table.prototype.testData = function(_) {
+	    this.columns(["Lat", "Long", "Pin"])
+	    this.data([
+	        [ 37.665074, -122.384375, "green-dot.png" ],
+	        [ 32.690680, -117.178540 ],
+	        [ 39.709455, -104.969859 ],
+	        [ 41.244123, -95.961610 ],
+	        [ 32.688980, -117.192040 ],
+	        [ 45.786490, -108.526600 ],
+	        [ 45.796180, -108.535652 ],
+	        [ 45.774320, -108.494370 ],
+	        [ 45.777062, -108.549835, "red-dot.png" ]
+	    ]);
+    	
+    	return this;
     };
 
     return Table;
