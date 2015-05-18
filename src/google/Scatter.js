548f185c88a7b77de6c091650d2b2caa01a690dd
unknown <SnelDa01@BCTDW7SNELDA01.risk.regn.net>
2015-05-18 12:13:48 -0400

    Fixes to Google Scatter and Timeline.  Adds Google TreeMap.

--- src/google/Scatter.js
+++ src/google/Scatter.js
@@ -1,80 +1,233 @@
 "use strict";
 (function (root, factory) {
     if (typeof define === "function" && define.amd) {
-        define(["d3", "./CommonND"], factory);
+        define(["d3", "./CommonND", "../common/HTMLWidget"], factory);
     } else {
-        root.Scatter = factory(root.d3, root.google_CommonND);
+        root.Scatter = factory(root.d3, root.google_CommonND, root.common_HTMLWidget);
     }
-}(this, function (d3, CommonND) {
+}(this, function (d3, CommonND, HTMLWidget) {
 
     function Scatter() {
         CommonND.call(this);
         this._class = "google_Scatter";
         this._tag = "div";
-    };
+        this._chartType = "ScatterChart";
+    }
     Scatter.prototype = Object.create(CommonND.prototype);
 
     /**
      * Publish Params Unique To This Widget
-     */   
+     */
     Scatter.prototype.publish("aggregationTarget", "auto", "string", "How multiple data selections are rolled up into tooltips: 'category'- Group selected data by x-value; 'series'- Group selected data by series; 'auto'- Group selected data by x-value if all selections have the same x-value, and by series otherwise; 'none'- Show only one tooltip per selection.  aggregationTarget will often be used in tandem with selectionMode and tooltip.trigger",null,{tags:['Basic']});
     
+    Scatter.prototype.publish("curveType", null, "string", "Controls the curve of the lines when the line width is not zero. Can be one of the following: 'none' - Straight lines without curve; 'function' - The angles of the line will be smoothed..",null,{tags:['Basic']});
+      
+    Scatter.prototype.publish("orientation", "horizontal", "string", "The orientation of the chart. When set to 'vertical', rotates the axes of the chart.",null,{tags:['Basic']});
+
+    Scatter.prototype.publish("pointShape", "circle", "string", "The shape of individual data elements: 'circle', 'triangle', 'square', 'diamond', 'star', or 'polygon'.",null,{tags:['Basic']});
+    
+    Scatter.prototype.publish("pointSize", 7, "number", "Diameter of data points, in pixels. Use zero to hide all points.",null,{tags:['Basic']});
+    
+    Scatter.prototype.publish("pointsVisible", true, "boolean", "Determines whether points will be displayed. Set to false to hide all points.",null,{tags:['Basic']});
+    
+    Scatter.prototype.publish("selectionMode", null, "string", "When selectionMode is 'multiple', users may select multiple data points.",null,{tags:['Basic']});
+    
+     /**
+     * Publish Params Common To Other Libraries
+     */
     Scatter.prototype.publish("backgroundColor", null, "html-color", "The background color for the main area of the chart. Can be either a simple HTML color string, for example: 'red' or '#00cc00', or an object with the following properties.",null,{tags:['Basic']});
-    
-    Scatter.prototype.publish("colors", null, "html-color", "The colors to use for the chart elements. An array of strings, where each element is an HTML color string, for example: colors:['red','#004411'].",null,{tags:['Basic']});
-    
-//    Scatter.prototype.publish("title", null, "string", "Text to display above the chart.",null,{tags:['Basic']});
-    
-    Scatter.prototype.publish("forceIFrame", false, "boolean", "Draws the chart inside an inline frame. (Note that on IE8, this option is ignored; all IE8 charts are drawn in i-frames.)",null,{tags:['Basic']});
-    
-    Scatter.prototype.publish("height", null, "number", "Height of the chart, in pixels.",null,{tags:['Basic']});
-    
-    Scatter.prototype.publish("hAxis", null, "object", "An object with members to configure various horizontal axis elements. To specify properties of this object, you can use object literal notation:  {title: 'Hello',  titleTextStyle: {color: '#FF0000'}}",null,{tags:['Basic']});
-    
-    Scatter.prototype.publish("vAxis", null, "object", "An object with members to configure various horizontal axis elements. To specify properties of this object, you can use object literal notation:  {title: 'Hello',  titleTextStyle: {color: '#FF0000'}}",null,{tags:['Basic']});
 
-    Scatter.prototype.publish("width", null, "number", "Width of the chart, in pixels.",null,{tags:['Basic']});
+    Scatter.prototype.publish("dataOpacity", 1.0, "number", "The transparency of data points, with 1.0 being completely opaque and 0.0 fully transparent. This refers to the visible data (i.e. dots).",null,{tags:['Basic']});
 
+    Scatter.prototype.publish("fontName", 'Arial', "string", "The default font face for all text in the chart. You can override this using properties for specific chart elements.",null,{tags:['Basic']});
+ 
+    Scatter.prototype.publish("axisFontSize", null, "number", "X/Y Axis Label Font Size",null,{tags:['Basic','Shared']});
+    Scatter.prototype.publish("axisFontFamily", null, "string", "X/Y Axis Label Font Name",null,{tags:['Basic','Shared']});
 
+    Scatter.prototype.publish("xAxisFontColor", null, "html-color", "X Axis Text Font Color",null,{tags:['Basic','Shared']});
+    Scatter.prototype.publish("yAxisFontColor", null, "html-color", "X Axis Text Font Color",null,{tags:['Basic','Shared']});
+
+    Scatter.prototype.publish("xAxisBaselineColor", null, "html-color", "Specifies The Color of The Baseline For The Horizontal Axis",null,{tags:['Intermediate','Shared']});
+    Scatter.prototype.publish("yAxisBaselineColor", null, "html-color", "Specifies The Color of The Baseline For The Vertical Axis",null,{tags:['Intermediate','Shared']});
+
+    Scatter.prototype.publish("xAxisTitleFontColor", null, "html-color", "Horizontal Axis Title Text Style (Color)",null,{tags:['Intermediate','Shared']});
+    Scatter.prototype.publish("yAxisTitleFontColor", null, "html-color", "Vertical Axis Title Text Style (Color)",null,{tags:['Intermediate','Shared']});
+
+    Scatter.prototype.publish("xAxisTitleFontSize", null, "number", "Horizontal Axis Titletext Style (Font Size)",null,{tags:['Intermediate','Shared']});
+    Scatter.prototype.publish("yAxisTitleFontSize", null, "number", "Vertical Axis Titletext Style (Font Size)",null,{tags:['Intermediate','Shared']});
+
+    Scatter.prototype.publish("xAxisTitleFontFamily", null, "string", "Horizontal Axis Title Text Style (Font Name)",null,{tags:['Intermediate','Shared']});
+    Scatter.prototype.publish("yAxisTitleFontFamily", null, "string", "Vertical Axis Title Text Style (Font Name)",null,{tags:['Intermediate','Shared']});
+
+    Scatter.prototype.publish("xAxisLabelRotation", 0, "number", "X Axis Label Angle",null,{tags:['Intermediate','Shared']});
+
+    Scatter.prototype.publish("xAxisBaseline", null, "number", "Specifies the color of the baseline for the horizontal axis",null,{tags:['Intermediate']});
+    Scatter.prototype.publish("yAxisBaseline", null, "number", "Specifies the color of the baseline for the vertical axis",null,{tags:['Intermediate']});
+    
+    Scatter.prototype.publish("xAxisInversed", false, "boolean", "The Direction In Which The Values Along The Horizontal Axis Grow.",null,{tags:['Advanced']});
+    Scatter.prototype.publish("yAxisInversed", false, "boolean", "The Direction In Which The Values Along The Vertical Axis Grow.",null,{tags:['Advanced']});
+
+    Scatter.prototype.publish("xAxisTitle", "", "string", "X Axis Title",null,{tags:['Basic','Shared']});
+    Scatter.prototype.publish("yAxisTitle", "", "string", "Y Axis Title",null,{tags:['Basic','Shared']});
+    
+    Scatter.prototype.publish("xAxisFormat", "", "string", "Format String For Numeric Axis Labels", ["","decimal","scientific","currency","percent","short","long"],{tags:['Intermediate']});
+    Scatter.prototype.publish("yAxisFormat", "", "string", "Format String For Numeric Axis Labels", ["","decimal","scientific","currency","percent","short","long"],{tags:['Intermediate']});
+
+    Scatter.prototype.publish("xAxisGridlinesCount", 5, "number", "The Number of Horizontal Gridlines Between Two Regular Gridlines",null,{tags:['Intermediate']});
+    Scatter.prototype.publish("yAxisGridlinesCount", 5, "number", "The Number of Vertical Gridlines Between Two Regular Gridline",null,{tags:['Intermediate']});
+
+    Scatter.prototype.publish("xAxisGridlinesColor", null, "html-color", "The Color of The Horizontal Gridlines Inside The Chart Area",null,{tags:['Basic']});
+    Scatter.prototype.publish("yAxisGridlinesColor", null, "html-color", "The Color of The Vertical Gridlines Inside The Chart Area",null,{tags:['Basic']});
+
+    Scatter.prototype.publish("xAxisMinorGridlinesCount", 0, "number", "The Number of Horizontal Minor Gridlines Between Two Regular Gridlines",null,{tags:['Intermediate']});
+    Scatter.prototype.publish("yAxisMinorGridlinesCount", 0, "number", "The Number of Vertical Minor Gridlines Between Two Regular Gridlines",null,{tags:['Intermediate']});
+
+    Scatter.prototype.publish("xAxisMinorGridlinesColor", null, "html-color", "The Color of The Horizontal Minor Gridlines Inside The Chart Area",null,{tags:['Intermediate']});
+    Scatter.prototype.publish("yAxisMinorGridlinesColor", null, "html-color", "The Color of The Vertical Minor Gridlines Inside The Chart Area",null,{tags:['Intermediate']});
+
+    Scatter.prototype.publish("xAxisLogScale", false, "boolean", "Makes Horizontal Axis A log Scale",null,{tags:['Advanced']});
+    Scatter.prototype.publish("yAxisLogScale", false, "boolean", "Makes Vertical Axis A Log Scale",null,{tags:['Advanced']});
+
+    Scatter.prototype.publish("xAxisTextPosition", "out", "set", "Position of The Horizontal Axis Text, Relative To The Chart Area", ["out","in","none"],{tags:['Advanced']});
+    Scatter.prototype.publish("yAxisTextPosition", "out", "set", "Position of The Vertical Axis Text, Relative To The Chart Area", ["out","in","none"],{tags:['Advanced']});
+
+    Scatter.prototype.publish("xAxisTicks", [], "array", "Replaces The Automatically Generated X-Axis Ticks With The Specified Array",null,{tags:['Private']});
+    Scatter.prototype.publish("yAxisTicks", [], "array", "Replaces The Automatically Generated Y-Axis Ticks With The Specified Array",null,{tags:['Private']});
+
+    Scatter.prototype.publish("xAxisMaxValue", null, "number", "Moves The Max Value of The Horizontal Axis To The Specified Value",null,{tags:['Advanced']});
+    Scatter.prototype.publish("yAxisMaxValue", null, "number", "Moves The Max Value of The Vertical Axis To The Specified Value",null,{tags:['Advanced']});
+
+    Scatter.prototype.publish("xAxisMinValue", null, "number", "Moves The Min Value of The Horizontal Axis To The Specified Value",null,{tags:['Advanced']});
+    Scatter.prototype.publish("yAxisMinValue", null, "number", "Moves The Min Value of The Vertical Axis To The Specified Value",null,{tags:['Advanced']});
+
+    Scatter.prototype.publish("xAxisViewWindowMode", "pretty", "set", "Specifies How To Scale The Horizontal Axis To Render The values Within The Chart Area", ["pretty","maximized","explicit"],{tags:['Advanced']});
+    Scatter.prototype.publish("yAxisViewWindowMode", "pretty", "set", "Specifies How To Scale The Vertical Axis To Render The Values Within The Chart Area", ["pretty","maximized","explicit"],{tags:['Advanced']});
+
+    Scatter.prototype.publish("xAxisViewWindowMax", null, "number", "The Maximum Horizontal Data Value To Render",null,{tags:['Advanced']});
+    Scatter.prototype.publish("yAxisViewWindowMax", null, "number", "The Maximum Vertical Data Value To Render",null,{tags:['Advanced']});
+
+    Scatter.prototype.publish("xAxisViewWindowMin", null, "number", "The Minimum Horizontal Data Value To Render",null,{tags:['Advanced']});
+    Scatter.prototype.publish("yAxisViewWindowMin", null, "number", "The Minimum Vertical Data Value To Render",null,{tags:['Advanced']});
 
     Scatter.prototype.getChartOptions = function () {
-        var retVal = []; 
+        var retVal = CommonND.prototype.getChartOptions.apply(this, arguments);
 
-        retVal.width = this.width();
-        retVal.height = this.height();
-        retVal.title = this.title();
-        retVal.hAxis = this.hAxis();
-        retVal.VAxis = this.vAxis();
+        retVal.aggregationTarget = this.aggregationTarget();
+        retVal.curveType = this.curveType();
+        retVal.orientation = this.orientation();
+        retVal.pointShape = this.pointShape();
+        retVal.pointSize = this.pointSize();
+        retVal.pointsVisible = this.pointsVisible();
+        retVal.selectionMode = this.selectionMode();
+        retVal.backgroundColor = this.backgroundColor();
+        retVal.dataOpacity = this.dataOpacity();
+        retVal.fontName = this.fontName();
 
-        return retVal;
+        retVal.hAxis = {};
+        retVal.vAxis = {};
+
+        // hAxis
+        retVal.hAxis.baseline = this.xAxisBaseline();
+        retVal.hAxis.gridlines = {
+            count: this.xAxisGridlinesCount(),
+            color: this.xAxisGridlinesColor()
+        };
+        retVal.hAxis.minorGridlines = {
+            count: this.xAxisMinorGridlinesCount(),
+            color: this.xAxisMinorGridlinesColor()
+        };
+        retVal.hAxis.logScale = this.xAxisLogScale();
+        retVal.hAxis.textPosition = this.xAxisTextPosition();
+        retVal.hAxis.minValue = this.xAxisMinValue();
+        retVal.hAxis.maxValue = this.xAxisMaxValue();
+        retVal.hAxis.slantedText = this.xAxisLabelRotation() !== 0;
+        retVal.hAxis.slantedTextAngle = this.xAxisLabelRotation();
+        retVal.hAxis.format = this.xAxisFormat();
+        retVal.hAxis.textStyle = {
+            color: this.xAxisFontColor(),
+            fontName: this.axisFontFamily() ? this.axisFontFamily() : this.fontFamily(),
+            fontSize: this.axisFontSize() ? this.axisFontSize() : this.fontSize()
+
+        };
+        if (this.xAxisTicks().length > 0) {
+            retVal.hAxis.ticks = this.xAxisTicks();
+        }
+        retVal.hAxis.titleTextStyle = {
+            color: this.xAxisTitleFontColor(),
+            fontName: this.xAxisTitleFontFamily(),
+            fontSize: this.xAxisTitleFontSize()
+        };
+        retVal.hAxis.viewWindowMode = this.xAxisViewWindowMode();
+        retVal.hAxis.viewWindow = {
+            min: this.xAxisViewWindowMin(),
+            max: this.xAxisViewWindowMax()
+        };
+
+        // vAxis
+        retVal.vAxis.baseline = this.yAxisBaseline();
+        retVal.vAxis.baselineColor = this.yAxisBaselineColor();
+        retVal.vAxis.direction = this.yAxisInversed() ? -1 : 1;
+        retVal.vAxis.gridlines = {
+            count: this.yAxisGridlinesCount(),
+            color: this.yAxisGridlinesColor()
+        };
+        retVal.vAxis.minorGridlines = {
+            count: this.yAxisMinorGridlinesCount(),
+            color: this.yAxisMinorGridlinesColor()
+        };
+        retVal.vAxis.logScale = this.yAxisLogScale();
+        retVal.vAxis.textPosition = this.yAxisTextPosition();
+        retVal.vAxis.title = this.yAxisTitle();
+        retVal.vAxis.minValue = this.yAxisMinValue();
+        retVal.vAxis.maxValue = this.yAxisMaxValue();
+        retVal.vAxis.format = this.yAxisFormat();
+        retVal.vAxis.textStyle = {
+            color: this.yAxisFontColor(),
+            fontName: this.axisFontFamily() ? this.axisFontFamily() : this.fontFamily(),
+            fontSize: this.axisFontSize() ? this.axisFontSize() : this.fontSize()
+        };
+        if (this.yAxisTicks().length > 0) {
+            retVal.vAxis.ticks = this.yAxisTicks();
+        }
+        retVal.vAxis.titleTextStyle = {
+            color: this.yAxisTitleFontColor(),
+            fontName: this.yAxisTitleFontFamily(),
+            fontSize: this.yAxisTitleFontSize()
+        };
+        retVal.vAxis.viewWindowMode = this.yAxisViewWindowMode();
+        retVal.vAxis.viewWindow = {
+            min: this.yAxisViewWindowMin(),
+            max: this.yAxisViewWindowMax()
+        };
     };
 
     Scatter.prototype.enter = function (domNode, element) {
-        element.style("overflow", "hidden");
-        
-        this.scatterChart = new google.visualization.ScatterChart(element.node());
+        CommonND.prototype.enter.apply(this, arguments); 
     };
 
     Scatter.prototype.update = function (domNode, element) {
-        this.scatterChart.draw(this._data_google, this.getChartOptions());
+        CommonND.prototype.update.apply(this, arguments);
     };
+    
     Scatter.prototype.data = function (_) {
+        var retVal = HTMLWidget.prototype.data.apply(this, arguments);
         if (arguments.length) {
-            this._data_google = new google.visualization.arrayToDataTable(_);
+            var data = null;
+            if (this._data.length) {
+                this._columns.shift();
+                this._data.forEach(function(value) {
+                    value.shift();
+                });
+                data = [this._columns].concat(this._data);
+            } else {
+                data = [
+                    ['', { role: 'annotation' }],
+                    ['', '']
+                ];
+            }
+            this._data_google = google.visualization.arrayToDataTable(data);
         }
-    };
-
-    Scatter.prototype.testData = function () {
-        this.data([
-            ['Age', 'Weight', 'Height'],
-            [ 8, 12, 15],
-            [ 4.2, 5.5, 7],
-            [ 11, 14, 18],
-            [ 4, 5, 6],
-            [ 3, 3.5, 4.5],
-            [ 15,17, 19]
-        ]);
-        return this;
+        return retVal;
     };
  
     return Scatter;
--- src/google/Timeline.js
+++ src/google/Timeline.js
@@ -12,6 +12,7 @@
         
         this._class = "google_Timeline";
         this._tag = "div";
+        this._chartType = "TimelineChart";
 
         this._data_google = [];
         this.columns([]);
@@ -22,61 +23,39 @@
     /**
      * Publish Params Common To Other Libraries
      */
-
+      Timeline.prototype.publish("tooltipIsHtml", "true", "boolean", "Set to false to use SVG-rendered (rather than HTML-rendered) tooltips. See Customizing Tooltip Content for more details.",null,{tags:['Advanced']});
     
+    Timeline.prototype.publish("tooltipTrigger", "focus", "string", "The user interaction that causes the tooltip to be displayed: 'focus' - The tooltip will be displayed when the user hovers over the element; 'none' - The tooltip will not be displayed.",null,{tags:['Basic']});
+    
+    Timeline.prototype.publish("backgroundColor", null, "html-color", "The background color for the main area of the chart. Can be either a simple HTML color string, for example: 'red' or '#00cc00'.",null,{tags:['Basic']});
+ 
     /**
      * Publish Params Unique To This Widget
      */   
     Timeline.prototype.publish("avoidOverlappingGridLines", true, "boolean", "Whether display elements (e.g., the bars in a timeline) should obscure grid lines. If false, grid lines may be covered completely by display elements. If true, display elements may be altered to keep grid lines visible.",null,{tags:['Basic']});
-    
-    Timeline.prototype.publish("backgroundColor", null, "html-color", "Horizontal axis text style (Font Size)",null,{tags:['Basic']});
-    
-    Timeline.prototype.publish("colors", null, "html-color", "The colors to use for the chart elements. An array of strings, where each element is an HTML color string, for example: colors:['red','#004411'].",null,{tags:['Basic']});
-    
-    Timeline.prototype.publish("enableInteractivity", true, "boolean", "Whether the chart throws user-based events or reacts to user interaction. If false, the chart will not throw 'select' or other interaction-based events (but will throw ready or error events), and will not display hovertext or otherwise change depending on user input.",null,{tags:['Advanced']});
-    Timeline.prototype.publish("forceIFrame", false, "boolean", "Draws the chart inside an inline frame. (Note that on IE8, this option is ignored; all IE8 charts are drawn in i-frames.)",null,{tags:['Basic']});
-    
-    Timeline.prototype.publish("height", null, "number", "Height of the chart, in pixels.",null,{tags:['Basic']});
-    
-    Timeline.prototype.publish("timeline.barLabelStyle", null, "object", "An object that specifies the bar label text style. It has this format: {color: <string>, fontName: <string>, fontSize: <string>}  The color can be any HTML color string, for example 'red' or '#00cc00'",null,{tags:['Basic']});
 
-    Timeline.prototype.publish("timeline.colorByRowLabel", false, "boolean", "If set to true, colors every bar on the row the same. The default is to use one color per bar label.",null,{tags:['Basic']});
+    Timeline.prototype.publish("timelineColorByRowLabel", false, "boolean", "If set to true, colors every bar on the row the same. The default is to use one color per bar label.",null,{tags:['Basic']});
     
-    Timeline.prototype.publish("timeline.groupByRowLabel", true, "boolean", "If set to false, creates one row for every dataTable entry. The default is to collect bars with the same row label into one row.",null,{tags:['Basic']});
+    Timeline.prototype.publish("timelineGroupByRowLabel", true, "boolean", "If set to false, creates one row for every dataTable entry. The default is to collect bars with the same row label into one row.",null,{tags:['Basic']});
+ 
+    Timeline.prototype.publish("timelineShowBarLabels", true, "boolean", "If set to false, omits bar labels. The default is to show them.", null,{tags:['Basic']});
     
-    Timeline.prototype.publish("timeline.rowLabelStyle", null, "object", "An object that specifies the row label text style. It has this format: {color: <string>, fontName: <string>, fontSize: <string>}  The color can be any HTML color string, for example 'red' or '#00cc00' ", null,{tags:['Basic']});
+    Timeline.prototype.publish("timelineShowRowLabels", true, "boolean", "If set to false, omits row labels. The default is to show them.",null,{tags:['Basic']});
     
-    Timeline.prototype.publish("timeline.showBarLabels", true, "boolean", "If set to false, omits bar labels. The default is to show them.", null,{tags:['Basic']});
-    
-    Timeline.prototype.publish("timeline.showRowLabels", true, "boolean", "If set to false, omits row labels. The default is to show them.",null,{tags:['Basic']});
-    
-    Timeline.prototype.publish("timeline.singleColor", null, "string", "Colors all bars the same. Specified as a hex value (e.g., '#8d8').",null,{tags:['Basic']});
-    
-    Timeline.prototype.publish("tooltip.isHtml", "true", "boolean", "Set to false to use SVG-rendered (rather than HTML-rendered) tooltips. See Customizing Tooltip Content for more details.",null,{tags:['Advanced']});
-    
-    Timeline.prototype.publish("tooltip.trigger", "focus", "string", "The user interaction that causes the tooltip to be displayed: 'focus' - The tooltip will be displayed when the user hovers over the element; 'none' - The tooltip will not be displayed.",null,{tags:['Basic']});
-    
-    Timeline.prototype.publish("width", null, "number", "Width of the chart, in pixels.",null,{tags:['Basic']});
+    Timeline.prototype.publish("timelineSingleColor", null, "string", "Colors all bars the same. Specified as a hex value (e.g., '#8d8').",null,{tags:['Basic']});
     
     Timeline.prototype.getChartOptions = function () {
         var retVal = [];
         
         retVal.avoidOverlappingGridLines = this.avoidOverlappingGridLines();
         retVal.backgroundColor = this.backgroundColor();
-        retVal.colors = this.colors();
-        retVal.enableInteractivity = this.enableInteractivity();
-        retVal.forceIFrame = this.forceIFrame();
-        retVal.height = this.height();
-//        retVal.timeline.barLabelStyle = this.timeline.barLabelStyle();
-//        retVal.timeline.colorByRowLabel = this.timeline.colorByRowLabel();
-//        retVal.timeline.groupByRowLabel = this.timeline.groupByRowLabel();
-//        retVal.timeline.rowLabelStyle = this.timeline.rowLabelStyle();
-//        retVal.timeline.showBarLabels = this.timeline.showBarLabels();
-//        retVal.timeline.showRowLabels = this.timeline.showRowLabels();
-//        retVal.timeline.singleColor = this.timeline.singleColor();
-//        retVal.tooltip.isHtml = this.tooltip.isHtml();
-//        retVal.tooltip.trigger = this.tooltip.trigger();
-        retVal.width = this.width();
+        retVal.timelineColorByRowLabel = this.timelineColorByRowLabel();
+        retVal.timelineGroupByRowLabel = this.timelineGroupByRowLabel();
+        retVal.timelineShowBarLabels = this.timelineShowBarLabels();
+        retVal.timelineShowRowLabels = this.timelineShowRowLabels();
+        retVal.timelineSingleColor = this.timelineSingleColor();
+        retVal.tooltipIsHtml = this.tooltipIsHtml();
+        retVal.tooltipTrigger = this.tooltipTrigger();
 
         return retVal;
     };
@@ -99,7 +78,7 @@
             this._data_google = new google.visualization.DataTable();
 
             this._data_google.addColumn({ type: 'string', id: 'Label A' });
-            this._data_google.addColumn({ type: 'string', id: 'Label B' }); // optional ?
+            this._data_google.addColumn({ type: 'string', id: 'Label B' }); // optional 
             this._data_google.addColumn({ type: 'date', id: 'start' });
             this._data_google.addColumn({ type: 'date', id: 'end' });
 
